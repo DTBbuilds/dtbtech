301b340fc0e223b3e1a805f5b13feed2b296fafb
DTBbuilds <dtbinfotech@gmail.com>
2025-09-16 15:29:32 +0300

    Fix build errors: resolve auth-modal.js and add type=module to script tags

---
+++ components/auth-modal.js
@@ -0,0 +1,11 @@
+// Empty auth modal component - functionality removed
+// This file exists to prevent build errors from legacy references
+
+export default null;
+export { AuthModal };
+
+class AuthModal {
+  constructor() {
+    // Placeholder class
+  }
+}
